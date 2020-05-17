f3c8b5325cade8c53544da824c79273c52b42470
Rui Shu <terry.shu@hotmail.com>
2020-05-17 16:36:32 -0400

    update

--- code/palindrome_soln.py
+++ code/palindrome_soln.py
@@ -1,17 +1,3 @@
-"""This module contains a code example related to
-
-Think Python, 2nd Edition
-by Allen Downey
-http://thinkpython2.com
-
-Copyright 2015 Allen Downey
-
-License: http://creativecommons.org/licenses/by/4.0/
-"""
-
-from __future__ import print_function, division
-
-
 def first(word):
     """Returns the first character of a string."""
     return word[0]
@@ -40,4 +26,3 @@ print(is_palindrome('allen'))
 print(is_palindrome('bob'))
 print(is_palindrome('otto'))
 print(is_palindrome('redivider'))
-
--- code/pi.py
+++ code/pi.py
@@ -1,16 +1,3 @@
-"""This module contains a code example related to
-
-Think Python, 2nd Edition
-by Allen Downey
-http://thinkpython2.com
-
-Copyright 2015 Allen Downey
-
-License: http://creativecommons.org/licenses/by/4.0/
-"""
-
-from __future__ import print_function, division
-
 import math
 
 
@@ -19,7 +6,7 @@ def factorial(n):
     if n == 0:
         return 1
     else:
-        recurse = factorial(n-1)
+        recurse = factorial(n - 1)
         result = n * recurse
         return result
 
@@ -34,15 +21,16 @@ def estimate_pi():
     k = 0
     factor = 2 * math.sqrt(2) / 9801
     while True:
-        num = factorial(4*k) * (1103 + 26390*k)
-        den = factorial(k)**4 * 396**(4*k)
+        num = factorial(4 * k) * (1103 + 26390 * k)
+        den = factorial(k) ** 4 * 396 ** (4 * k)
         term = factor * num / den
         total += term
-        
+
         if abs(term) < 1e-15:
             break
         k += 1
 
     return 1 / total
+
 
 print(estimate_pi())
